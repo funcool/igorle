d113eb1c244981bedb9d877f1e51f5f72e31f4fb
Andrey Antukh <niwi@niwi.nz>
2015-09-20 15:02:36 +0300

    Update dependencies.

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :source-paths ["src"]
-  :jar-exclusions [#"\.swp|\.swo|user.clj"]
+  :jar-exclusions [#"\.swp|\.swo|user\.clj"]
   :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
+  :plugins [[lein-ancient "0.6.7"]]
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.7.28" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.122" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [funcool/postal-parser "0.1.0-alpha1"]
-                 [funcool/promesa "0.3.0"]
-                 [funcool/cats "0.5.0"]
-                 [funcool/cuerdas "0.5.0"]
-                 [com.taoensso/timbre "4.0.2"]])
+                 [funcool/postal "0.2.0-SNAPSHOT"]
+                 [funcool/promesa "0.5.0"]
+                 [funcool/cats "1.0.0"]
+                 [funcool/cuerdas "0.6.0"]])
